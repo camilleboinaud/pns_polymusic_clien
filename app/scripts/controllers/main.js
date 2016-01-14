80bfee875ddf0d6feaecf02056d103d8fa6214f0
Camille Boinaud <camille.boinaud@gmail.com>
2016-01-14 12:04:35 +0100

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name pnsPolymusicClientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the pnsPolymusicClientApp
+ */
+angular.module('pnsPolymusicClientApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
