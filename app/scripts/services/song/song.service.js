1aa9cc691bf2d167c10b7b7668987795dcd2442a
jonathan <jonathan.pujol@etu.unice.fr>
2016-01-21 11:11:23 +0100

    on enleve le .idea

--- app/scripts/services/song/song.service.js
+++ app/scripts/services/song/song.service.js
@@ -9,11 +9,11 @@ angular.module('pnsPolymusicClientApp').factory('songFactory', function () {
       tracks: [
         {
           name: 'piano',
-          url: 'http://192.168.43.219:3000/api/songs/569fb4cc41beec772adda858/tracks/569fb4cc41beec772adda859'
+          url: 'http://localhost:9000/music/queen_champions/voix.mp3'
         },
         {
           name: 'bass',
-          url: 'http://192.168.43.219:3000/api/songs/569fb4cc41beec772adda858/tracks/569fb4cc41beec772adda85a'
+          url: 'http://localhost:9000/music/queen_champions/basse.mp3'
         }
       ]
     }
