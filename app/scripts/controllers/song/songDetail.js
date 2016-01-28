440954073291da20ded7f1a234487158918c3e4c
Tianhao Shi <shi.tianhao@etu.unice.fr>
2016-01-28 11:57:28 +0100

    comment with server

--- app/scripts/controllers/song/songDetail.js
+++ app/scripts/controllers/song/songDetail.js
@@ -8,10 +8,39 @@
  * Controller of the pnsPolymusicClientApp
  */
 angular.module('pnsPolymusicClientApp')
-  .controller('SongDetailCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('SongDetailCtrl', function ($scope, $routeParams, SongREST) {
+
+    /**
+     * INIT: Get song's info by ID
+     */
+    SongREST.getSongUrlById($routeParams.songId, function (song) {
+      $scope.song = song;
+    });
+
+
+    var getAllComments = function () {
+      SongREST.getAllCommentsBySongId($routeParams.songId, function (comments) {
+        $scope.comments = comments;
+        $scope.nbComment = comments.length;
+      });
+    };
+    getAllComments();
+
+    $scope.newComment = function () {
+      console.log('newComment');
+      var params = {
+        songId: $routeParams.songId,
+        content: $scope.content
+      };
+      SongREST.writeNewComments(params, function (response) {
+        console.log(response);
+        getAllComments();
+        $scope.content = '';
+      })
+    }
+
+
+
+
+
   });
--- app/scripts/services/api/songrestapi.service.js
+++ app/scripts/services/api/songrestapi.service.js
@@ -9,6 +9,11 @@ angular.module('pnsPolymusicClientApp').factory('SongREST', ['$http', 'Upload',
       serverAddress = 'http://localhost:3000';
 
     SongREST.serverAddress = serverAddress;
+
+      /**
+       * Get list of pub songs
+       * @param callback
+       */
     SongREST.getAllPubSongs = function (callback) {
       $http({
         method: 'GET',
@@ -20,15 +25,68 @@ angular.module('pnsPolymusicClientApp').factory('SongREST', ['$http', 'Upload',
       });
     };
 
-    SongREST.getSongUrlById = function (ID) {
-      return serverAddress+'/api/songs/'+ID;
+    /**
+     * Get song by ID
+     * @param ID
+     * @param callback
+       */
+    SongREST.getSongUrlById = function (ID, callback) {
+      $http({
+        method: 'GET',
+        url: serverAddress+'/api/songs/'+ID
+      }).then(function successCallback(response){
+        callback(response.data);
+      }, function errorCallback(error) {
+        console.log(error);
+      });
     };
 
+    /**
+     * Upload song with multi tracks
+     * @param song
+     * @param successCallback
+     * @param errorCallback
+     * @param progressCallback
+       */
     SongREST.uploadSong = function (song, successCallback, errorCallback, progressCallback) {
       Upload.upload({
         url: serverAddress + '/api/songs',
         data: { songName:song.name, author:song.author, file: song.tracks}
       }).then(successCallback, errorCallback, progressCallback);
+    };
+
+
+    /**
+     * Get all song's comments
+     * @param ID
+     * @param callback
+       */
+    SongREST.getAllCommentsBySongId = function (ID, callback) {
+      $http({
+        method: 'GET',
+        url: serverAddress+'/api/songs/'+ID+'/comments'
+      }).then(function successCallback(response){
+        callback(response.data);
+      }, function errorCallback(error) {
+        console.log(error);
+      });
+    };
+
+    /**
+     * Write new comment for a song
+     * @param params
+     * @param callback
+     */
+    SongREST.writeNewComments = function (params, callback) {
+      $http({
+        method: 'POST',
+        url: serverAddress+'/api/comments',
+        data:{songId: params.songId, content:params.content}
+      }).then(function successCallback(response){
+        callback(response.data);
+      }, function errorCallback(error) {
+        console.log(error);
+      });
     };
 
     return SongREST;
